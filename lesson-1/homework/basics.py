b6d07d6adc04566d9e552abbadc5ff04dfdb8899
Giizcode <farahmukumova@mail.ru>
2025-04-26 20:41:20 +0500

    Create basics.py

---
+++ lesson-1/homework/basics.py
@@ -0,0 +1,31 @@
+#1.Given a side of square. Find its perimeter and area.
+a=6 
+P=4*a #perimeter
+Area=a*a
+print ('Perimeter',P)
+print('Area', Area)
+
+#2.Given diameter of circle. Find its length.
+p=3.14
+d=5
+C=p*d
+print('Circumference',C)
+
+#3.Given two numbers a and b. Find their mean.
+a=145
+b=421
+Mean=a+b/2
+print('Mean',Mean)
+
+#4.Given two numbers a and b. Find their sum, product and square of each number.
+a=8
+b=9
+SUM=a+b
+Product=a*b
+Square_a=a*a
+Square_b=b*b
+
+print('Sum',SUM)
+print('Product',Product)
+print('Square_a',Square_a)
+print('Square_b',Square_b)
